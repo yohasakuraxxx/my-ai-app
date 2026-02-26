c2743554dac09d54a3438fc2a67e673b0daca81b
yohasakuraxxx <os.sos.swan@gmail.com>
2026-02-26 22:40:14 +0900

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Time API"}
+    return {"message": "Hello my first ai app"}
 
 @app.get("/time")
 def get_current_time():
